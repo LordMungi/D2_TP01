7d7fb441d5a1ad2a10193663ee271154fad6f62f
LordMungi <lrdmungi@gmail.com>
2026-04-21 00:21:29 -0300

    Finish ship movement system

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -11,6 +11,6 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * SPEED * Time.deltaTime);
+        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Forward")) * SPEED * Time.deltaTime);
     }
 }
