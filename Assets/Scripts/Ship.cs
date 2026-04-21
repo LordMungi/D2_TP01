42297fe5ef9351f4b39c5f1d122d6fc0c54354da
LordMungi <lrdmungi@gmail.com>
2026-04-21 01:04:34 -0300

    Add rotation to ship

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
     [SerializeField] float SPEED;
+    [SerializeField] float SENSITIVITY;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Forward")) * SPEED * Time.deltaTime);
+        transform.rotation *= Quaternion.AngleAxis(SENSITIVITY * Time.deltaTime, new Vector3(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), -Input.GetAxisRaw("Roll")));
     }
 }
